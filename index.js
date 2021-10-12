a5f25d88c4713391708ebaa831c17d061065efe7
Erez Friemagor <erezfree29@gmail.com>
2021-10-12 21:05:10 +0100

    add moving java_script skills

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ document.querySelector(".fa-bars").addEventListener("click", function() {
     if (x.style.display === "none") {
       x.style.display = "block";
       y.style.marginTop = "30px";
-      z.style.top = "44vh";
+      z.style.top = "35vh";
     } else {
       x.style.display = "none";
       y.style.marginTop = "1px";
-      z.style.top = "39vh";
+      z.style.top = "30vh";
     }
   });
+
+
+  setInterval(function(){ 
+
+    let x = document.querySelector('.web_developer');
+    let y = document.querySelector('.ruby_developer');
+    let  z = document.querySelector('.java_script');
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      z.style.display = "none";
+      y.style.display = "block";
+
+    } else if (y.style.display === "block"){
+      x.style.display = "none";
+      y.style.display = "none";
+      z.style.display = "block";
+    } else {
+      x.style.display = "block";
+      y.style.display = "none";
+      z.style.display = "none";
+    }
+
+}, 3000);
