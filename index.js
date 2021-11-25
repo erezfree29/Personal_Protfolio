cafd7946ee15509952928bfb90b96609b0183a59
Erez Friemagor <erezfree29@gmail.com>
2021-11-25 10:05:35 +0000

    coursel

--- index.js
+++ index.js
@@ -19,19 +19,56 @@ document.querySelector(".fa-bars").addEventListener("click", function() {
     let x = document.querySelector('.web_developer');
     let y = document.querySelector('.ruby_developer');
     let  z = document.querySelector('.java_script');
+    let  r = document.querySelector('.react');
+    let  red = document.querySelector('.redux');
     if (x.style.display === "block") {
       x.style.display = "none";
-      z.style.display = "none";
       y.style.display = "block";
 
     } else if (y.style.display === "block"){
-      x.style.display = "none";
       y.style.display = "none";
       z.style.display = "block";
-    } else {
-      x.style.display = "block";
-      y.style.display = "none";
+    } else if (z.style.display === "block"){
+      r.style.display = "block";
       z.style.display = "none";
+    }
+    else if (r.style.display === "block") {
+      r.style.display = "none";
+      red.style.display = "block";
+    }
+
+    else {
+      red.style.display = "none";
+      x.style.display = "block";
     }
 
 }, 3000);
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector("nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+
+
+
+
+
